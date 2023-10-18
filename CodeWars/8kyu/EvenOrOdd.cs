e8662d4036ee3b59db900c8ca00df1003cb92a2f
Joshua Latusia <joshuaRMS@users.noreply.github.com>
2023-10-18 18:25:43 +0200

    Added EvenOrOdd kata

---
+++ CodeWars/8kyu/EvenOrOdd.cs
@@ -0,0 +1,19 @@
+﻿/*
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/csharp
+
+8 kyu
+Even or Odd
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+namespace CodeWars
+{
+    public class EvenOrOdd
+    {
+        public static string CalculateEvenOrOdd(int number)
+        {
+            return (number % 2 == 0) ? "Even" : "Odd";
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/EvenOrOddTests.cs
@@ -0,0 +1,48 @@
+﻿using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+
+    [TestFixture]
+    public class EvenOrOddTests
+    {
+        [TestCase(1)]
+        [TestCase(7)]
+        public void PositiveOddTest(int num)
+        {
+            Assert.That(EvenOrOdd.CalculateEvenOrOdd(num), Is.EqualTo("Odd"));
+        }
+
+        [TestCase(2)]
+        [TestCase(42)]
+        public void PositiveEvenTest(int num)
+        {
+            Assert.That(EvenOrOdd.CalculateEvenOrOdd(num), Is.EqualTo("Even"));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-7)]
+        public void NegativeOddTest(int num)
+        {
+            Assert.That(EvenOrOdd.CalculateEvenOrOdd(num), Is.EqualTo("Odd"));
+        }
+
+        [TestCase(-2)]
+        [TestCase(-42)]
+        public void NegativeEvenTest(int num)
+        {
+            Assert.That(EvenOrOdd.CalculateEvenOrOdd(num), Is.EqualTo("Even"));
+        }
+
+        [Test]
+        public void ZeroIsEvenTest()
+        {
+            Assert.That(EvenOrOdd.CalculateEvenOrOdd(0), Is.EqualTo("Even"));
+        }
+    }
+}
